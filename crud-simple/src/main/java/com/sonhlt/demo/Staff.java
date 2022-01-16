5abb569db23c7efefe471024437bd920b3ef6ad3
Son Le <leson1998ls@gmail.com>
2022-01-16 18:15:19 +0700

    first commit

---
+++ crud-simple/src/main/java/com/sonhlt/demo/Staff.java
@@ -0,0 +1,47 @@
+package com.sonhlt.demo;
+
+public class Staff {
+    public Staff(int id, String name, String department, int salary) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    private int id;
+    private String name;
+    private String department;
+    private int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
---
+++ crud-simple/src/main/java/com/sonhlt/demo/StaffController.java
@@ -0,0 +1,60 @@
+package com.sonhlt.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffController {
+    //tạo list để lưu trữ dữ liệu staff
+    static List<Staff> staffList = new ArrayList<>();
+    //khởi tạo dữ liệu cho list
+    static {
+        staffList.add(new Staff(1, "Son", "IT", 100));
+        staffList.add(new Staff(2, "Hoang", "Accounting", 400));
+        staffList.add(new Staff(3, "Le", "Station", 250));
+        staffList.add(new Staff(4, "Thanh", "BOD", 700));
+    }
+
+    @GetMapping()
+    public List<Staff> getAllStaffs() {
+        return staffList;
+    }
+
+    @GetMapping("/{id}")
+    public Staff getStaffById(@PathVariable  int id) {
+        return this.getStaff(id);
+    }
+
+    private Staff getStaff(int id) {
+        for (Staff staff : staffList) {
+            if (staff.getId() == id) {
+                return staff;
+            }
+        }
+        //Không nên return null
+        return null;
+    }
+
+    @PostMapping()
+    public List<Staff> addStaff(@RequestBody Staff staff) {
+        staffList.add(staff);
+        return staffList;
+    }
+
+    @PutMapping("/{id}")
+    public List<Staff> updateStaff(@PathVariable int id, @RequestBody Staff staff) {
+        staffList.remove(getStaff(id));
+        staff.setId(id);
+        staffList.add(staff);
+        return staffList;
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Staff> deleteStaff(@PathVariable int id) {
+        staffList.remove(getStaff(id));
+        return staffList;
+    }
+}
